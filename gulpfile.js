449b7994781e0f8c3b3faea293047a16df770610
Monki <monki@loveknitting.com>
2017-11-17 14:35:24 +0000

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+
+gulp.task('minify-css', () => {
+	gulp.src('site/css/main.css')
+		.pipe(cleanCSS())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('./site/css/'))
+});
+
+gulp.task('styles', function() {
+    gulp.src('site/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./site/css/'))
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('site/scss/**/*.scss',['styles']);
+    gulp.watch('site/css/main.css',['minify-css']);
+});
